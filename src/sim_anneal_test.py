e71f070f5546ec3b2bb7391a4a18ecca44aa80e1
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2016-07-31 13:07:43 +0200

    Change test to multidimensional input

--- src/sim_anneal_test.py
+++ src/sim_anneal_test.py
@@ -7,15 +7,13 @@ from simulatedAnnealing import simulated_annealing
 
 
 def error(x):
-    return 0.3*sin(x) + sin(1.3*x) + 0.9*sin(4.2*x) + 0.002*random()
+    return 0.3*sin(x[0]) + sin(1.3*x[0]) + 0.9*sin(4.2*x[0]) + x[1]**2 + 0.002*random()
 
 
 def neighbour(x):
-    y = x + uniform(-1, 1)
-    if y > 10:
-        y = 10
-    if y < 0:
-        y = 0
+    y = map(lambda x: x + uniform(-1, 1), x)
+    y = map(lambda x: 10 if x > 10 else x, y)
+    y = map(lambda x: 0 if x < 0 else x, y)
     return y
 
 
@@ -31,9 +29,9 @@ def stop_condition(t):
 
 
 def main():
-    init_pos = -10
+    init_pos = [-10, 5]
     init_error = error(init_pos)
-    best = simulated_annealing(init_pos, init_error, error, neighbour, acceptance, stop_condition, 100.0)
+    best = simulated_annealing(init_pos, init_error, error, neighbour, acceptance, stop_condition, 100.0, lambda x: 0.98*x)
     print best
 
 if __name__ == "__main__":
