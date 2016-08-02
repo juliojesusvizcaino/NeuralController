8a12036fc2fa174ae077aaa15209311b224d4732
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2016-08-02 10:39:11 +0200

    Add main function documentation

--- src/simulatedAnnealing.py
+++ src/simulatedAnnealing.py
@@ -9,6 +9,27 @@ from random import random
 
 def simulated_annealing(init, init_error, error, neighbour, acceptance, stop_condition,
                         init_temp, temperature_update=lambda x: x * 0.95):
+    """
+    Global minimum search using simulated annealing
+
+    :param init: Initial parameters
+    :type init: whatever
+    :param init_error: Initial error
+    :type init_error: float
+    :param error: Error function
+    :type error: function output_error = error(params)
+    :param neighbour: Give neighbours of received param
+    :type neighbour: function iterable = neighbour(params)
+    :param acceptance: Probability of change when error is worse
+    :type acceptance: function probability = acceptance(error_diff, temperature)
+    :param stop_condition: Condition to stop at
+    :type stop_condition: function boolean = stop_condition(temperature)
+    :param init_temp: Temperature at which start
+    :type init_temp: float
+    :param temperature_update: Way of update temperature
+    :type temperature_update: function new_temp = temperature_update(temp)
+    :return: Optimum parameters
+    """
     if stop_condition(init_temp):
         return init
     else:
