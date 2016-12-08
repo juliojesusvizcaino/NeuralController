b9bc104f4cdacd3768b6fde968aad68c057da5d6
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2016-12-08 18:10:16 +0100

    Add position and velocity preparation

--- src/model_selection.py
+++ src/model_selection.py
@@ -141,24 +141,34 @@ def main():
     x_target = np.array(target_pos)
     x_first = np.array([pos_[0] for pos_ in pos])
     x_speed = np.array(target_speed).reshape((-1, 1))
-    aux_output = [np.ones(eff.shape[0]).reshape((-1, 1)) for eff in effort]
+    aux_output = [np.ones(eff_.shape[0]).reshape((-1, 1)) for eff_ in effort]
 
     x = np.concatenate((x_target, x_first, x_speed), axis=1)
 
+    def prepare_time_data(data):
+        data_scaler = StandardScaler()
+        data_concat = np.concatenate([data_ for data_ in data], axis=0)
+        data_scaler.fit(data_concat)
+        new_data = [data_scaler.transform(data_) for data_ in data]
+
+        return data_scaler, new_data
+
     input_scaler = StandardScaler()
     x = input_scaler.fit_transform(x)
-    output_scaler = StandardScaler()
-    effort_concat = np.concatenate([a for a in effort], axis=0)
-    output_scaler.fit(effort_concat)
-    effort = [output_scaler.transform(eff) for eff in effort]
+    effort_scaler, effort = prepare_time_data(effort)
+    pos_scaler, pos = prepare_time_data(pos)
+    vel_scaler, vel = prepare_time_data(vel)
 
-    y = pad_sequences(effort, padding='post', value=0.)
+    torque = pad_sequences(effort, padding='post', value=0.)
+    pos = pad_sequences(pos, padding='post', value=0.)
+    vel = pad_sequences(vel, padding='post', value=0.)
     aux_output = pad_sequences(aux_output, padding='post', value=0.)
-    y_mask = aux_output[:,:,0]
-    y_aux_mask = np.ones(aux_output.shape[:2])
+    mask = aux_output[:,:,0]
+    aux_mask = np.ones(aux_output.shape[:2])
 
-    x, x_test, y, y_test, y_aux, y_aux_test, y_mask, y_test_mask, y_aux_mask, y_aux_test_mask = \
-        train_test_split(x, y, aux_output, y_mask, y_aux_mask, test_size=0.3, random_state=seed)
+    x, x_test, torque, torque_test, pos, pos_test, vel, vel_test, \
+        aux, aux_test, mask, mask_test, aux_mask, aux_mask_test = \
+        train_test_split(x, torque, pos, vel, aux_output, mask, aux_mask, test_size=0.3, random_state=seed)
 
     kf = KFold(n_splits=3, shuffle=True, random_state=seed)
 
@@ -173,9 +183,9 @@ def main():
         img_names = ['imgs/' + name + '/' for name in names]
 
         this_x, this_y, this_y_aux, this_y_mask, this_y_aux_mask = \
-            [aux[train_index] for aux in [x, y, y_aux, y_mask, y_aux_mask]]
+            [a_[train_index] for a_ in [x, torque, aux, mask, aux_mask]]
         this_x_cv, this_y_cv, this_y_aux_cv, this_y_mask_cv, this_y_aux_mask_cv = \
-            [aux[cv_index] for aux in [x, y, y_aux, y_mask, y_aux_mask]]
+            [a_[cv_index] for a_ in [x, torque, aux, mask, aux_mask]]
 
         widths_gru = [10, 10, 100, 100]
         depths_gru = [1, 2, 1, 2]
@@ -184,7 +194,7 @@ def main():
                 zip(widths_gru, depths_gru, save_names, log_names, img_names):
             model = MyModel(train=[this_x, [this_y, this_y_aux]], val=[this_x_cv, [this_y_cv, this_y_aux_cv]],
                             train_mask=[this_y_mask, this_y_aux_mask], val_mask=[this_y_mask_cv, this_y_aux_mask_cv],
-                            test=[x_test, [y_test, y_aux_test]], test_mask=[y_test_mask, y_aux_test_mask],
+                            test=[x_test, [torque_test, aux_test]], test_mask=[mask_test, aux_mask_test],
                             max_unroll=n_rollout, save_dir=save_name, log_dir=log_name, img_dir=img_name,
                             width_gru=width_gru, depth_gru=depth_gru, width_dense=50, depth_dense=2, optimizer='adam')
             if args.train:
